22fd1b85f5869a5000fa28f5c3611b4438de5574
RxCu <88845105+RxCu@users.noreply.github.com>
2025-09-16 10:22:55 -0700

    Add Jumping

--- Unity-Demo/Assets/Scripts/PlayerController.cs
+++ Unity-Demo/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
+using System;
 
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
+  public bool setTag = true;
   public float speed = 5f;
   public float jumpHeight = 10f;
+  public int maxJumps = 2;
+  public float validJumpDistance = 1.0f;
 
   public double maxHealth = 100f;
   public double defaultHealth = 100f;
@@ -19,21 +24,17 @@ public class PlayerController : MonoBehaviour {
   public bool paused = true;
   public bool firstPerson = true;
 
-  public float yawSensitivity = 30f;
-  public float pitchSensitivity = 30f;
-
-  public Vector2 cameraPitchLimits = new Vector2(-90.0f, 90.0f);
-  public Vector3 cameraOffset = new Vector3(0.0f, 0.5f, 0.5f);
-
   public Vector3 respawnPoint = new Vector3(0.0f, 1.0f, 0.0f);
-  public bool setTag = true;
+
+  InputAction jumpAction;
+  int jumpNum = 0;
 
   Vector2 moveVector;
-  Vector3 jumpForce;
 
-  public Transform cameraAnchor;
-  InputAction lookVector;
-  Vector2 cameraRotation;
+  Camera mainCamera;
+  CinemachineBrain cinemachineBrain;
+
+  RaycastHit hit;
 
   Rigidbody rb;
 
@@ -56,23 +57,37 @@ public class PlayerController : MonoBehaviour {
       this.gameObject.tag = "Player";
     }
 
-    this.rb = this.GetComponent<Rigidbody>();
     this.health = new PlayerHealth(this.maxHealth, this.defaultHealth);
 
+    this.rb = this.GetComponent<Rigidbody>();
     this.moveVector = new Vector2(0, 0);
-    this.lookVector = InputSystem.actions.FindAction("Look");
+    this.jumpAction = InputSystem.actions.FindAction("Jump");
 
-    this.cameraRotation = Vector2.zero;
+    // Initialize (Cinemachine) Camera
+    if(this.cinemachineBrain == null) {
+      if(CinemachineBrain.ActiveBrainCount < 0) {
+        throw new MissingMemberException("Could not find Cinemachine Brain");
+      }
 
-    if(this.cameraAnchor == null) {
-      this.cameraAnchor = this.transform.GetChild(0);
+      this.cinemachineBrain = CinemachineBrain.GetActiveBrain(0);
     }
+
+    this.mainCamera = this.cinemachineBrain.OutputCamera;
+
+    // Initialize Raycast
+    this.hit = new RaycastHit();
 
     if(this.paused) {
       this.Pause();
-    } else {
+    }
+    else {
       this.Unpause();
     }
+  }
+
+  void OnValidate() {
+    this.health.maxHealth = this.maxHealth;
+    this.health.defaultHealth = this.defaultHealth;
   }
 
   void Update() {
@@ -87,32 +102,30 @@ public class PlayerController : MonoBehaviour {
       return;
     }
 
-    if(this.UpdatePaused()) {
-      this.UpdateRotation();
-      return;
-    }
+    if(this.UpdatePaused()) return;
 
-    this.UpdateLook();
     this.UpdateRotation();
-
     this.UpdateMovement();
-  }
-
-  void OnValidate() {
-    this.jumpForce = new Vector3(0, this.jumpHeight, 0);
-    this.health.maxHealth = this.maxHealth;
-    this.health.defaultHealth = this.defaultHealth;
   }
 
   bool UpdatePaused() {
     // TODO: add proper pause actions
     if(Input.GetKey(KeyCode.Escape)) {
       this.Pause();
-    } else if(Input.GetKey(KeyCode.Space)) {
+    } else if(Input.GetKey(KeyCode.Return)) {
       this.Unpause();
     }
 
     return this.paused;
+  }
+
+  void UpdateRotation() {
+    // set player yaw
+    Quaternion rot = this.mainCamera.transform.rotation;
+    rot.x = 0;
+    rot.z = 0;
+
+    this.transform.rotation = rot;
   }
 
   void UpdateMovement() {
@@ -125,36 +138,42 @@ public class PlayerController : MonoBehaviour {
     this.rb.linearVelocity = (velocity.x * transform.forward) +
                              (velocity.y * transform.up) +
                              (velocity.z * transform.right);
+
+    // TODO: fix this jump check system
+    if(Physics.Raycast(this.transform.position, -this.transform.up,
+        out this.hit, this.validJumpDistance)) {
+
+      if(hit.collider == null) return;
+
+      this.jumpNum = 0;
+
+      if(this.jumpAction.phase == InputActionPhase.Performed) {
+        this.Jump();
+      }
+    }
   }
 
-  void UpdateLook() {
-    //this.cameraAnchor.position = this.transform.position + this.cameraOffset;
-
-    Vector2 input = this.lookVector.ReadValue<Vector2>();
-
-    this.cameraRotation.x += input.x * this.yawSensitivity * Time.deltaTime;
-    this.cameraRotation.y = Mathf.Clamp(this.cameraRotation.y +
-      input.y * this.pitchSensitivity * Time.deltaTime, this.cameraPitchLimits.x, this.cameraPitchLimits.y);
-  }
-
-  void UpdateRotation() {
-    // set player yaw
-    this.transform.localRotation = Quaternion.AngleAxis(this.cameraRotation.x, Vector3.up);
-
-    // set camera rotation
-    this.cameraAnchor.rotation = Quaternion.Euler(-this.cameraRotation.y, this.cameraRotation.x, 0);
-  }
-
-  public void Move(InputAction.CallbackContext ctx) {
+  public void OnMove(InputAction.CallbackContext ctx) {
     Vector2 input = ctx.ReadValue<Vector2>();
 
     this.moveVector.x = input.y;
     this.moveVector.y = input.x;
   }
 
-  public void Jump(InputAction.CallbackContext ctx) {
-    // TODO: check if on ground and input was down
-    this.rb.AddForce(this.jumpForce);
+  public void OnJump(InputAction.CallbackContext ctx) {
+    if(ctx.phase == InputActionPhase.Canceled) {
+      return;
+    }
+
+    this.Jump();
+  }
+
+  void Jump() {
+    if(this.jumpNum >= this.maxJumps) return;
+
+    // TODO: account for gravity (esp with air jumps)
+    this.rb.AddForce(this.transform.up * this.jumpHeight);
+    this.jumpNum++;
   }
 
   void OnTriggerEnter(Collider other) {
